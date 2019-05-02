4384e43dbf5924aa03b5e0c0e272ba98ef080d14
wlau903 <wlau903@gmail.com>
2019-05-02 15:07:19 -0700

    New header/home components

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render() {
+    return(
+      <div>
+        <h2>Hungry?</h2>
+        <p>Click above to start your order.</p>
+      </div>
+    )
+  }
+
+}
+
+export default Home;
