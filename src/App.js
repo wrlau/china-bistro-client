a3b8bed626169c0eb6da78943c3e49dbd9b400e3
wlau903 <wlau903@gmail.com>
2019-05-02 15:05:10 -0700

    Add react-router and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import OrderForm from './containers/OrderForm';
 import Dishes from './components/Dishes';
 import DishService from './services/DishService';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -20,18 +24,17 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>China Bistro</h1>
-        <div className="nav-bar">
-          <p>nav bar</p>
-        </div>
-        <div className="order-form">
-          <OrderForm />
-        </div>
-        <div className="side-bar">
-          <Dishes dishes={this.state.dishes}/>
-        </div>
-      </div>
+      <Router>
+        <React.Fragment>
+          <div className="App">
+            <Header />
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/menu" render={() => <Dishes dishes={this.state.dishes} /> } />
+            <Route exact path="/order/new" component={OrderForm} />
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
